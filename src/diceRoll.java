4d3e6322cf0711a3edf4ffacb9fcb101b129c386
David Smiley <DGSmiley77@gmail.com>
2016-09-13 15:41:49 -0400

    first commit

---
+++ src/diceRoll.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class diceRoll {
+		
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to the Grand Circus Casino! ");
+		
+		System.out.print("Enter number of sides for dice: ");
+		int dice = input.nextInt();
+
+		System.out.println("Roll the dice? (yes/no)");
+	}
+	}
+
+	
